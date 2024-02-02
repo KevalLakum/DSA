0388f3daaf64bda20af9a715185e8e2965406207
unknown <lakumkeval2004@gmail.com>
2024-02-02 10:15:35 +0530

    cpp

---
+++ c.cpp
@@ -0,0 +1,20 @@
+ #include<iostream>
+ using namespace std;
+
+ int main(){
+   int a;
+   int b;
+   int c;
+
+    cout << "enter value of a:";
+    cin >> a ;
+
+     cout << "enter value of b:";
+    cin >> b ;
+
+    c = a + b;
+
+    cout << "Sum is " << c;
+
+    return 0;
+ }
